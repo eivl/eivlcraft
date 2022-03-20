5ca3eaafb287197f5e78d464dd709dc27a51b700
Eivind Teig <eivind.teig@gmail.com>
2022-03-20 18:31:30 +0100

    Add different block faces.

--- block_type.py
+++ block_type.py
@@ -12,5 +12,28 @@ class BlockType:
         self.indices = numbers.indices
         self.text_coords = numbers.tex_coords[:]
 
-        for texture in block_face_texture.values():
+        def set_block_face(side, texture):
+            for vertex in range(4):
+                self.text_coords[side * 12 + vertex * 3 + 2] = texture
+
+        for face, texture in block_face_texture.items():
             texture_manager.add_texture(texture)
+            texture_index = texture_manager.textures.index(texture)
+
+            if face == 'all':
+                set_block_face(0, texture_index)
+                set_block_face(1, texture_index)
+                set_block_face(2, texture_index)
+                set_block_face(3, texture_index)
+                set_block_face(4, texture_index)
+                set_block_face(5, texture_index)
+
+            elif face == 'sides':
+                set_block_face(0, texture_index)
+                set_block_face(1, texture_index)
+                set_block_face(4, texture_index)
+                set_block_face(5, texture_index)
+
+            else:
+                set_block_face(["right", "left", "top", "bottom", "front",
+                                "back"].index(face), texture_index)
