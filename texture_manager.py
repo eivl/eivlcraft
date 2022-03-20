f0fd9ad328b5b8deb376d909b270186e37516a4f
Eivind Teig <eivind.teig@gmail.com>
2022-03-20 17:22:07 +0100

    Add texture array.

---
+++ texture_manager.py
@@ -0,0 +1,22 @@
+import ctypes
+import pyglet
+import pyglet.gl as gl
+
+
+class TextureManager:
+    def __init__(self, texture_width, texture_height, max_textures):
+        self.texture_width = texture_width
+        self.texture_height = texture_height
+        self.max_texture = max_textures
+
+        self.texture = []
+
+        self.texture_array = gl.GLuint(0)
+        gl.glGenTextures(1, self.texture_array)
+        gl.glBindTexture(gl.GL_TEXTURE_2D_ARRAY, self.texture_array)
+
+        gl.glTexImage3D(
+            gl.GL_TEXTURE_2D_ARRAY, 0, gl.GL_RGBA,
+            self.texture_width, self.texture_height, self.max_texture,
+            0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, None
+        )
