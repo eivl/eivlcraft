5d2da865f105b2e825553c2a82bc7372fd9c755f
Eivind Teig <eivind.teig@gmail.com>
2022-03-20 20:10:09 +0100

    Add Shading to grass block.

--- frag.glsl
+++ frag.glsl
@@ -6,7 +6,8 @@ uniform sampler2DArray texture_array_sampler;
 
 in vec3 local_position;
 in vec3 interpolated_tex_coords;
+in float interpolated_shading_value;
 
 void main(void) {
-    fragment_colour = texture(texture_array_sampler, interpolated_tex_coords);
+    fragment_colour = texture(texture_array_sampler, interpolated_tex_coords) * interpolated_shading_value;
 }
